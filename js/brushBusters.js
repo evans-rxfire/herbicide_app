ef24f12cfe9adffab8fc36275642fda4604cec55
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2025-11-11 19:38:15 -0600

    2025.11.11 Working on Brush Busters object and HTML input form.

---
+++ js/brushBusters.js
@@ -0,0 +1,110 @@
+const brushBustersReccomends = {
+    "Mesquite": {
+        "Leaf": {
+
+        },
+        "Stem": {
+
+        }
+    },
+    "Huisache": {
+        "Leaf": {
+
+        },
+        "Stem": {
+
+        }
+    },
+    "Prickly Pear": {
+        "Pad and Stem": {
+
+        }
+    },
+    "Ashe Juniper": {
+        "Leaf": {
+
+        },
+        "Spot": {
+
+        }
+    },
+    "Blueberry Juniper": {
+        "Leaf": {
+
+        },
+        "Spot": {
+
+        }
+    },
+    "Chinese Tallow": {
+        "Leaf": {
+
+        },
+        "Stem": {
+
+        }
+    },
+    "Greenbriar": {
+        "Stem": {
+
+        }
+    },
+    "Texas Persimmon": {
+        "Leaf": {
+
+        },
+        "Stem": {
+
+        }
+    },
+    "Agarita": {
+        "Leaf": {
+
+        }
+    },
+    "Bumelia": {
+        "leaf": {
+
+        }
+    },
+    "Catclaw Acacia": {
+        "Leaf": {
+
+        }
+    },
+    "Catclaw Mimosa": {
+        "Leaf": {
+            
+        }
+    },
+    "Coyotillo": {
+        "Leaf": {
+
+        }
+    },
+    "Flameleaf Sumac": {
+        "Leaf": {
+
+        }
+    },
+    "Lotebush": {
+        "Leaf": {
+
+        }
+    },
+    "Tasajillo": {
+        "Leaf": {
+
+        }
+    },
+    "Texas Mountain Laurel": {
+        "Leaf": {
+
+        }
+    },
+    "Whitebrush": {
+        "Leaf": {
+
+        }
+    }
+};
