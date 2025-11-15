865e3473e1ea1374baa6219738e0545806ed85a6
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2025-11-15 13:50:08 -0600

    2025.11.15 Building brush busters reccomendations object.

--- js/brushBusters.js
+++ js/brushBusters.js
@@ -1,7 +1,76 @@
-const brushBustersReccomends = {
+const brushBustersReccomendations = {
     "mesquite": {
         "Leaf": {
-
+            "Works well on": "Plants that have a good leaf canopy and are less than 6ft tall",
+            "When to apply": "When leaves change from light pea-green to uniform dark-green. Continue through July 31 for East and South Texas, rest of state through September",
+            "Ingredient": {
+                "Herbicide1": {
+                    "Name": "Sendero",
+                    "Concentration": "1%",
+                    "Tank Size": {
+                        "1 gal": "1.28 oz",
+                        "3 gal": "4 oz",
+                        "14 gal": "18 oz",
+                        "25 gal": "32 oz"
+                    },
+                    "Surfactant": "Yes",
+                    "Dye": "Yes"
+                },
+                "Herbicide2": {
+                    "Name": "Invora",
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    },
+                    "Surfactant": "Yes",
+                    "Dye": "Yes"
+                },
+                "Herbidice3": {
+                    "Name": ["Remedy Ultra", "Clear Pasture", "Triclopyr R&P", "Triclopyr 4EC"],
+                    "Concentration": "0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.64 oz",
+                        "3 gal": "2 oz",
+                        "14 gal": "9 oz",
+                        "25 gal": "16 oz"
+                    },
+                    "Surfactant": "Yes",
+                    "Dye": "Yes"
+                },
+                "Herbicide4": {
+                    "Name": ["Pyramid R&P", "Clopyralid 3"],
+                    "Concentration": "0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.64 oz",
+                        "3 gal": "2 oz",
+                        "14 gal": "9 oz",
+                        "25 gal": "16 oz"
+                    },
+                    "Surfactant": "Yes",
+                    "Dye": "Yes"
+                },
+                "Surfactant": {
+                "Concentration": "0.25%",
+                "Tank Size": {
+                    "1 gal": "0.32 oz",
+                    "3 gal": "1 oz",
+                    "14 gal": "4.5 oz",
+                    "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concetration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            },
         },
         "Stem": {
 
@@ -59,52 +128,352 @@ const brushBustersReccomends = {
     },
     "agarita": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "bumelia": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "catclaw-acacia": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "catclaw-mimosa": {
         "Leaf": {
-            
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "coyotillo": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "flameleaf-sumac": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "lotebush": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "tasajillo": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "texas-mountain-laurel": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     },
     "whitebrush": {
         "Leaf": {
-
+            "Works well on": "Plants that have good leaf canopy and are less than 6ft tall",
+            "When to apply": "Begin in late Spring through Summer with mature, healthy leaves. Plants should have full leaf canopy and good soil moisture",
+            "Ingredient": {
+                "Invora": {
+                    "Concentration": "1.5%",
+                    "Tank Size": {
+                        "1 gal": "1.92 oz",
+                        "3 gal": "5.76 oz",
+                        "14 gal": "27 oz",
+                        "25 gal": "48 oz"
+                    }
+                },
+                "Non-ionic surfactant": {
+                    "Concentration": "0.25%",
+                    "Tank Size": {
+                        "1 gal": "0.32 oz",
+                        "3 gal": "1 oz",
+                        "14 gal": "4.5 oz",
+                        "25 gal": "8 oz"
+                    }
+                },
+                "Hi-Light &trade Blue Dye": {
+                    "Concentration": "0.25-0.5%",
+                    "Tank Size": {
+                        "1 gal": "0.32-0.64 oz",
+                        "3 gal": "1-2 oz",
+                        "14 gal": "4.5-9 oz",
+                        "25 gal": "8-16 oz"
+                    }
+                }
+            }
         }
     }
 };
